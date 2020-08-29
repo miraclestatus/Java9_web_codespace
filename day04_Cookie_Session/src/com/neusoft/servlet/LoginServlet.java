cb9cbc521e9354e2e6c54309de6e291f1a60c31d
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-08-29 14:44:24 +0800

    111

---
+++ day04_Cookie_Session/src/com/neusoft/servlet/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.neusoft.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author Eric Lee
+ * @date 2020/8/29 14:07
+ */
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 设置request编码
+        req.setCharacterEncoding("utf-8");
+        // 获取参数
+
+
+
+
+
+
+    }
+}
