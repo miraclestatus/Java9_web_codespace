f877048f9a83724bb782c6189a3410a7ff83bbcb
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-08-29 09:04:00 +0800

    1111

---
+++ day04_Cookie_Session/src/com/neusoft/cookie/CookieDemo1.java
@@ -0,0 +1,34 @@
+package com.neusoft.cookie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Eric Lee
+ * @date 2020/8/29 08:54
+ */
+@WebServlet("/cookieDemo1")
+public class CookieDemo1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 创建cookie对象
+        Cookie cookie = new Cookie("id", "aaaadlddddddssssssss");
+        // 发送cookie
+        resp.addCookie(cookie);
+
+        // 获取cookie
+
+
+
+    }
+}
