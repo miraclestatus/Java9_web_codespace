7fb3bad97cfe8da8850a090a1b4f0f8ba3fd8344
miraclestatus <31105601+miraclestatus@users.noreply.github.com>
2020-08-29 14:15:58 +0800

    1111

---
+++ day04_Cookie_Session/src/com/neusoft/session/SessionDemo1.java
@@ -0,0 +1,30 @@
+package com.neusoft.session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author Eric Lee
+ * @date 2020/8/29 14:07
+ */
+@WebServlet("/sessionDemo1")
+public class SessionDemo1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 获取session
+        HttpSession session = req.getSession();
+        // 2.存储数据
+        session.setAttribute("msg", "hello session");
+
+    }
+}
---
+++ day04_Cookie_Session/src/com/neusoft/session/SessionDemo2.java
@@ -0,0 +1,33 @@
+package com.neusoft.session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+/**
+ * @author Eric Lee
+ * @date 2020/8/29 14:07
+ */
+@WebServlet("/sessionDemo2")
+public class SessionDemo2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 1. 获取session
+        HttpSession session = req.getSession();
+        // 2. 获取数据
+        Object msg = session.getAttribute("msg");
+        System.out.println(msg);
+        // 期望客户端关闭后， session也能相同
+        Cookie cookie = new Cookie("JSEESSIONID", session.getId());
+        cookie.setMaxAge(60);
+        resp.addCookie(cookie);
+
+
+    }
+}
